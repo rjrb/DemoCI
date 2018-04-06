923c2aeff05d11a620acd74185f070bddf41fa6f
Ricardo Ramírez <rickyrb@gmail.com>
2018-04-06 17:50:13 -0500

    * Sugerencias SonarQube

--- src/main/java/com/ramirezblauvelt/democi/beans/PaisSoportado.java
+++ src/main/java/com/ramirezblauvelt/democi/beans/PaisSoportado.java
@@ -33,7 +33,7 @@ public class PaisSoportado {
 				'}';
 	}
 
-	private static class FromDate {
+	private static class RangeDate {
 		private int day;
 		private int month;
 		private int year;
@@ -63,34 +63,10 @@ public class PaisSoportado {
 		}
 	}
 
-	private static class ToDate {
-		private int day;
-		private int month;
-		private int year;
+	private static class FromDate extends RangeDate {
+	}
 
-		public int getDay() {
-			return day;
-		}
-
-		public void setDay(int day) {
-			this.day = day;
-		}
-
-		public int getMonth() {
-			return month;
-		}
-
-		public void setMonth(int month) {
-			this.month = month;
-		}
-
-		public int getYear() {
-			return year;
-		}
-
-		public void setYear(int year) {
-			this.year = year;
-		}
+	private static class ToDate extends RangeDate {
 	}
 
 	public String getCountryCode() {
