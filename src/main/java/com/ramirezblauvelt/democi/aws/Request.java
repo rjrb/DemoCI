145551ea082b6888ec76580efc0e6d31bd246fcc
Ricardo Ramírez <rickyrb@gmail.com>
2018-05-26 11:59:57 -0500

    * Se define la forma de serializar y deserializar la clase LocalDate desde Jackson

--- src/main/java/com/ramirezblauvelt/democi/aws/Request.java
+++ src/main/java/com/ramirezblauvelt/democi/aws/Request.java
@@ -1,9 +1,22 @@
 package com.ramirezblauvelt.democi.aws;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Request {
 
+	@JsonDeserialize(using = LocalDateDeserializer.class)
+	@JsonSerialize(using = LocalDateSerializer.class)
 	private LocalDate fechaInicial;
 	private int diasHabilesSumar;
 	private String pais;
@@ -39,6 +52,39 @@ public class Request {
 
 	public void setPais(String pais) {
 		this.pais = pais;
+	}
+
+	/**
+	 * Permite de-serializar la clase LocalDate desde Jackson (para AWS)
+	 */
+	public class LocalDateDeserializer extends StdDeserializer<LocalDate> {
+
+		private static final long serialVersionUID = 1L;
+
+		protected LocalDateDeserializer() {
+			super(LocalDate.class);
+		}
+
+		@Override
+		public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt)
+			throws IOException {
+			return LocalDate.parse(jp.readValueAs(String.class));
+		}
+
+	}
+
+	public class LocalDateSerializer extends StdSerializer<LocalDate> {
+
+		private static final long serialVersionUID = 1L;
+
+		public LocalDateSerializer() {
+			super(LocalDate.class);
+		}
+
+		@Override
+		public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider sp) throws IOException {
+			gen.writeString(value.format(DateTimeFormatter.ISO_LOCAL_DATE));
+		}
 	}
 
 }
